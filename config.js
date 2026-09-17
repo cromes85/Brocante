80f3567d2cd096221171decc7c14752920a68275
cromes85 <idv230685@gmail.com>
2026-09-17 20:26:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// Aucune clé privée ni aucun mot de passe dans ce fichier.
+// Aucune clé privée ni aucun mot de passe ici.
 window.BROCANTE_CONFIG = {
-  // Coller ici l’URL /exec après le déploiement Google Apps Script.
-  apiUrl: "",
-  sheetUrl: "https://docs.google.com/spreadsheets/d/1a1C3NJFDnlJgHLUonnntC3DbZbD2wBY8V8TcqD11niU/edit",
-  refreshSeconds: 30
+ apiUrl: "https://script.google.com/macros/s/AKfycbyWpaVc1by4I5m_yyjaZaDti-7uNibu3fZw4T-Ytr41fjvVLQ7c1L1xysoDkyyafSHk/exec",
+ sheetUrl: "https://docs.google.com/spreadsheets/d/1a1C3NJFDnlJgHLUonnntC3DbZbD2wBY8V8TcqD11niU/edit",
+ refreshSeconds: 30
 };
