e11d134f3e2ef4586c0f973af1059fe4a4603987
cromes85 <idv230685@gmail.com>
2026-09-25 22:36:46 +0200

    Fix SyntaxError by removing duplicate isTargetSector declaration and exporting CLEAN_VIEWS and mapMode on window

--- sector-ui.js
+++ sector-ui.js
@@ -1,4 +1,5 @@
-let mapMode='ensemble';
+var mapMode='ensemble';
+window.mapMode=mapMode;
 
 // Transformations affines pour chaque vue
 const BANTIGNY_TRANSFORM=affine([[279,72],[1185,830],[120,250]],[[530,492],[595,564],[514,518]].map(p=>project(PLAN_TRANSFORM,p)));
@@ -9,6 +10,7 @@ const CLEAN_VIEWS={
   deschamps_plan:{file:'deschamps-plan.png',name:'Rue Deschamps (Plan)',matrix:affine([[487/1024,79/724],[813/1024,318/724],[931/1024,709.5/724]],[[288.1,272.0],[409.4,378.3],[456.2,542.6]].map(p=>project(PLAN_TRANSFORM,p)))},
   delfosse:{file:'delfosse-sans-voitures.png',name:'Rue Delfosse',matrix:affine([[546/1024,90/724],[364/1024,350/724],[523/1024,557/724]],[[431.3,146.6],[297.1,336.5],[414.1,487.9]].map(p=>project(PLAN_TRANSFORM,p)))}
 };
+window.CLEAN_VIEWS=CLEAN_VIEWS;
 
 function globalToMap(p){
   const cv=CLEAN_VIEWS[mapMode];
@@ -168,7 +170,6 @@ function drawZones(){
     g.onpointerdown=e=>e.stopPropagation();
     g.onkeydown=e=>{if(e.key==='Enter'||e.key===' '){e.preventDefault();chooseSector(s.id)}};
 
-    const isTargetSector = sectorEl.value ? (sectorEl.value===s.id) : (s.id===(CLEAN_VIEWS[mapMode]?mapMode:(mapMode==='gare'?'gare':'gare')));
     if(window.ADMIN_MODE && window.sectorEditMode && isTargetSector){
       const isPolygon = !!(outline && outline.length);
       const targetPaths = isPolygon ? [poly] : [band];
